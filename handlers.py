c9968f64cb3816d802cdf23d5775b46faa092c40
Ярослав Васильев <vasilyev@auto.local>
2023-05-24 14:20:55 +0300

    подготовка основы

---
+++ handlers.py
@@ -0,0 +1,23 @@
+from aiogram import types, F, Router
+from aiogram.types import Message
+from aiogram.filters import Command
+
+import kb
+import text
+
+router = Router()
+
+@router.message(Command('start'))
+async def start_handler(msg: Message):
+    await msg.answer(text.greet.format(name=msg.from_user.full_name), reply_markup=kb.menu)
+
+
+@router.message(F.text == "Меню")
+@router.message(F.text == "меню")
+@router.message(F.text == "Выйти в меню")
+@router.message(F.text == "◀️ Выйти в меню")
+async def menu(msg: Message):
+    await msg.answer(text.menu, reply_markup=kb.menu)
+# @router.message()
+# async def message_handler(msg: Message):
+#     await msg.answer(f'Твой АйДи: {msg.from_user.id}')
