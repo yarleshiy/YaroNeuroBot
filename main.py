9bd6be7e51bed53b6c594e225f5a03d8d32227f6
vasilyev <yaroslav@vasilyev.ru>
2023-05-29 11:24:44 +0300

    add link with openai

--- main.py
+++ main.py
@@ -18,6 +18,6 @@ async def main():
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
     asyncio.run(main())
 
